b8d8346c97b6925f825931b96cb5382ea5bda25e
Zachary Bell <z.bell@me.com>
2019-04-24 05:00:59 +0000

    secure user deletes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.order("places").paginate(page: params[:page], per_page: 5)
@@ -31,7 +31,7 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.update_attributes(place_params)
     redirect_to root_path
 
@@ -39,6 +39,10 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+      
     @place.destroy
     redirect_to root_path
   end
