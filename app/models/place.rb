5f7b8f8156e7777af4eab825f3a8f19cdec6699c
Zachary Bell <z.bell@me.com>
2019-04-29 02:53:53 +0000

    Comments db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   validates :name, presence: true, length: { minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
+  
   geocoded_by :address
   after_validation :geocode
   
