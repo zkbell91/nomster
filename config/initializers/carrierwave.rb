65ec67b1aabb15b779acdfaab6487b8628de1430
Zachary Bell <z.bell@me.com>
2019-05-21 23:14:59 +0000

    successfully upload and display images from AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 # config/initializers/carrierwave.rb
-
+require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
