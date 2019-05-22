583a38dda13f823f50b3927e48534a5d8045d107
Zachary Bell <z.bell@me.com>
2019-05-22 04:21:23 +0000

    adjust mailer for Heroku

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   
-  def comment_added (comment)
+  def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
     mail(to: @place_owner.email,
